331556a95a2291d052323aa0efd86a6729730e52
Shaikh Maviya <maviyashk25@gmail.com>
2026-07-06 12:43:29 +0530

    Time: 35 ms (44.24%) | Memory: 46.4 MB (33.82%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -5,7 +5,6 @@ class Solution {
         int n = s.length();
         int res = 0;
 
-        // Try all possible distinct counts
         for (int d = 1; d <= 26; d++) {
             Map<Character, Integer> freq = new HashMap<>();
             int left = 0, right = 0;
@@ -18,7 +17,6 @@ class Solution {
                 if (freq.get(c) == k) countAtLeastK++;
                 right++;
 
-                // Shrink if too many distinct chars
                 while (distinct > d) {
                     char leftChar = s.charAt(left);
                     if (freq.get(leftChar) == k) countAtLeastK--;
@@ -30,7 +28,6 @@ class Solution {
                     left++;
                 }
 
-                // Valid window: all distinct chars appear at least k times
                 if (distinct == countAtLeastK) {
                     res = Math.max(res, right - left);
                 }
