c2aa5aeba78cb1be73fda37021d54e27f8ef8462
Shaikh Maviya <maviyashk25@gmail.com>
2026-07-11 11:28:33 +0530

    Time: 8 ms (38.25%) | Memory: 47.1 MB (58.13%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int count = 0;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (set.contains(curr.val) &&
+                (curr.next == null || !set.contains(curr.next.val))) {
+                count++;
+            }
+            curr = curr.next;
+        }
+        return count;
+    }
+}
